7443965eb05e8ba3c84d9c108e2fb9d53dd59b94
moaaztaha <moaaztaha7@gmail.com>
2021-04-05 01:48:40 +0200

    more cores

--- datasets.py
+++ datasets.py
@@ -11,41 +11,40 @@ import spacy
 
 spacy_en = spacy.load('en')
 
+
 class FlickrDataset(Dataset):
     def __init__(self, root_dir, captions_file, vocab, transforms=None, freq_threshold=2, split='train'):
         # split has to be one of {train, val, test}
         assert split in {'train', 'val', 'test'}
-        
+
         self.root_dir = root_dir
         self.df = pd.read_csv(captions_file)
         self.vocab = vocab
         self.transforms = transforms
-        
-        
+
         # getting image ids and captions based on split
         self.df = self.df[self.df['split'] == split]
         self.img_idx = self.df['file_name'].values
         self.captions = self.df['caption'].values
-        
-        
+
         # printing some info
         print(f"Dataset split: {split}")
         print(f"Unique Image: {self.df.file_name.nunique()}")
         print(f"Size: {self.df.shape[0]}")
-    
+
     def __len__(self):
         return self.df.shape[0]
-    
+
     def __getitem__(self, index):
         caption = self.captions[index]
         img_id = self.img_idx[index]
         img = Image.open(self.root_dir+img_id).convert("RGB")
-        
+
         if self.transforms is not None:
             img = self.transforms(img)
         else:
             img = transforms.ToTensor()(img)
-        
+
         # numericalize captions
         word_idx = self.vocab.numericalize(caption)
         caption = [self.vocab.stoi["<sos>"]]
@@ -53,8 +52,7 @@ class FlickrDataset(Dataset):
         caption.append(self.vocab.stoi["<eos>"])
         return img, torch.tensor(caption)
 
-    
-    
+
 class Vocabulary:
     def __init__(self, freq_threshold=2):
         self.freq_threshold = freq_threshold
@@ -101,40 +99,44 @@ def build_vocab(data_file, freq_threshold=2, split='train'):
 
     return vocab
 
+
 class collate_fn:
     def __init__(self, pad_idx):
         self.pad_idx = pad_idx
-        
+
     def __call__(self, batch):
         images = [item[0].unsqueeze(0) for item in batch]
         images = torch.cat(images, dim=0)
-    
+
         captions = [item[1] for item in batch]
-        captions = pad_sequence(captions, batch_first=False, padding_value=self.pad_idx)
+        captions = pad_sequence(
+            captions, batch_first=False, padding_value=self.pad_idx)
 
         return images, captions
 
+
 def get_loaders(bs, images_path, df_path, transform, vocab):
     pad_idx = vocab.stoi['<pad>']
-    
+
     train_loader = DataLoader(
-    dataset=FlickrDataset(images_path, df_path,transforms=transform, vocab=vocab, split='train'),
-    batch_size=bs,
-    num_workers=0,
-    shuffle=True,
-    pin_memory=True,
-    collate_fn=collate_fn(pad_idx),
-    drop_last=True
-)
+        dataset=FlickrDataset(images_path, df_path,
+                              transforms=transform, vocab=vocab, split='train'),
+        batch_size=bs,
+        num_workers=2,
+        shuffle=True,
+        pin_memory=True,
+        collate_fn=collate_fn(pad_idx),
+        drop_last=True
+    )
     valid_loader = DataLoader(
-    dataset=FlickrDataset(images_path, df_path, transforms=transform, vocab=vocab, split='val'),
-    batch_size=bs,
-    num_workers=0,
-    shuffle=True,
-    pin_memory=True,
-    collate_fn=collate_fn(pad_idx),
-    drop_last=True
-)
-    
+        dataset=FlickrDataset(images_path, df_path,
+                              transforms=transform, vocab=vocab, split='val'),
+        batch_size=bs,
+        num_workers=2,
+        shuffle=True,
+        pin_memory=True,
+        collate_fn=collate_fn(pad_idx),
+        drop_last=True
+    )
+
     return train_loader, valid_loader
-    
