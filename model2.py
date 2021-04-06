849948995c1f4be5bbf7d7d2f99801d618af778c
moaaztaha <moaaztaha7@gmail.com>
2021-04-06 15:55:41 +0200

    teacher forcing

--- model2.py
+++ model2.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,75 +5,102 @@ import torch.optim as optim
 import torchvision.models as models
 
 
-# LSTM + full teacher forcing
-# encoder CNN
-class EncoderCNN(nn.Module):
-    def __init__(self, hid_dim, dropout, train_cnn=False):
-        super().__init__()
-
-        self.hid_dim = hid_dim
-        self.train_cnn = train_cnn
-
-        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
-        self.inception.fc = nn.Linear(self.inception.fc.in_features, hid_dim)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, images):
-        features = self.dropout(self.relu(self.inception(images)))
-        return features
-
-
-# decoder LSTM
-class DecoderLSTM(nn.Module):
-    def __init__(self, emb_dim, hid_dim, vocab_size, dropout):
-        super().__init__()
-
-        self.hid_dim = hid_dim
-        self.embedding = nn.Embedding(vocab_size, emb_dim)
-        self.rnn = nn.LSTM(emb_dim, hid_dim)
-        self.linear = nn.Linear(hid_dim, vocab_size)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, features, captions):
-        embeddings = self.dropout(self.embedding(captions))
-        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
-        rnn_out, _ = self.rnn(embeddings)
-        outputs = self.linear(rnn_out)
-        return outputs
-
-# decoder GRU
-
-
 class DecoderRNN(nn.Module):
-    def __init__(self, emb_dim, hid_dim, vocab_size, dropout):
+    def __init__(self, emb_dim, hid_dim, vocab_sz, dropout):
         super().__init__()
 
         self.hid_dim = hid_dim
-        self.embedding = nn.Embedding(vocab_size, emb_dim)
-        self.rnn = nn.GRU(emb_dim, hid_dim)
-        self.linear = nn.Linear(hid_dim, vocab_size)
+        self.vocab_sz = vocab_sz
+
+        self.embedding = nn.Embedding(vocab_sz, emb_dim)
+
+        # LSTM: inputs-> [embeddings + context], output -> hidden, output
+        self.rnn = nn.LSTM(emb_dim+hid_dim, hid_dim)
+
+        # FC: inputs:-> [embeddings + context + output]
+        self.fc_out = nn.Linear(emb_dim + hid_dim*2, vocab_sz)
+
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, features, captions):
-        embeddings = self.dropout(self.embedding(captions))
-        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
-        rnn_out, _ = self.rnn(embeddings)
-        outputs = self.linear(rnn_out)
-        return outputs
+    def forward(self, input, hidden, context):
+        # input = [batch size]
+        # hidden = [1, batch size, hid dim]
+        # context = [1, batch size, hid dim]
+
+        input = input.unsqueeze(0)
+        # input = [1, batch size]
+
+        embedded = self.dropout(self.embedding(input))
+        # embedded = [1, batch size, emb dim]
+
+#         print(f'Context shape {context.shape}')
+#         print(f'embeded shape {embedded.shape}')
+
+        emb_con = torch.cat((embedded, context), dim=2)
+        # emb_con = [1, batch size, emb_dim + hid_dim]
+
+#         print(f"Hidden shape {hidden.shape}")
+#         print(f"embedded shape {embedded.shape}")
+#         print(f"context shape {context.shape}")
+#         print(f"emb_con shape {emb_con.shape}")
+#         print('_'*22)
+
+        output, hidden = self.rnn(emb_con, hidden)
+        # output = [1, batch size, hid dim] -> seq len and n directions = 1
+        # hidden = [1, batch size, hid dim] -> n layers and n directions =1
+
+        output = torch.cat(
+            (embedded.squeeze(0), hidden.squeeze(0), context.squeeze(0)), dim=1)
+        # output = [batch size, emb_dim + hid_dim*2]
+
+        predictions = self.fc_out(output)
+        # predictions = [batch size, vocab size]
+
+        return predictions, hidden
+
+###
+# No teacher forcing for now
+###
 
 
 class Img2Seq(nn.Module):
-    def __init__(self, encoder, decoder, device):
+    def __init__(self, encoder, decoder, tf_ratio=0.5, device):
         super().__init__()
 
+        self.tf_ratio = tf_ratio
         self.encoder = encoder
         self.decoder = decoder
         self.device = device
 
-        assert self.encoder.hid_dim == self.decoder.hid_dim
+        assert self.encoder.hid_dim == self.decoder.hid_dim,\
+            'The hid dim of the context vector must equal the hid dim of the decoder !!'
 
-    def forward(self, images, captions):
-        features = self.encoder(images)
-        outputs = self.decoder(features, captions)
+    def forward(self, img, trg):
+        # src = [batch size, 3, 224, 224]
+        # trg = [trg len, batch size]
+
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.vocab_sz
+
+        outputs = torch.zeros(trg_len, batch_size,
+                              trg_vocab_size).to(self.device)
+
+        context = self.encoder(img)
+        context = context.unsqueeze(0)
+        hidden = context
+
+        input = trg[0, :]
+
+        for t in range(1, trg_len):
+            output, hidden = self.decoder(input, hidden, context)
+
+            outputs[t] = output
+
+            top1 = output.argmax(1)
+
+            teacher_force = random.random() < self.tf_ratio
+
+            input = trg[y] if teacher_force else top1
+
         return outputs
---
+++ models.py
@@ -0,0 +1,80 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+
+
+# LSTM + full teacher forcing
+# encoder CNN
+class EncoderCNN(nn.Module):
+    def __init__(self, hid_dim, dropout, train_cnn=False):
+        super().__init__()
+
+        self.hid_dim = hid_dim
+        self.train_cnn = train_cnn
+
+        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        self.inception.fc = nn.Linear(self.inception.fc.in_features, hid_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, images):
+        features = self.dropout(self.relu(self.inception(images)))
+        return features
+
+
+# decoder LSTM
+class DecoderLSTM(nn.Module):
+    def __init__(self, emb_dim, hid_dim, vocab_size, dropout):
+        super().__init__()
+
+        self.hid_dim = hid_dim
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim)
+        self.linear = nn.Linear(hid_dim, vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, features, captions):
+        embeddings = self.dropout(self.embedding(captions))
+        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+        rnn_out, _ = self.rnn(embeddings)
+        outputs = self.linear(rnn_out)
+        return outputs
+
+# decoder GRU
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, emb_dim, hid_dim, vocab_size, dropout):
+        super().__init__()
+
+        self.hid_dim = hid_dim
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.rnn = nn.GRU(emb_dim, hid_dim)
+        self.linear = nn.Linear(hid_dim, vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, features, captions):
+        embeddings = self.dropout(self.embedding(captions))
+        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+        rnn_out, _ = self.rnn(embeddings)
+        outputs = self.linear(rnn_out)
+        return outputs
+
+
+class Img2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+        assert self.encoder.hid_dim == self.decoder.hid_dim
+
+    def forward(self, images, captions):
+        features = self.encoder(images)
+        outputs = self.decoder(features, captions)
+        return outputs
